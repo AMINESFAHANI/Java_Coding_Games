60ca88e0d318fb20b41b50a96a230756cb4f7832
Amin <amin.esfahani.85@gmail.com>
2022-03-03 13:26:39 -0800

    3.3.2022

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args){
+
+        ProductOfDigitsOfSum obj= new ProductOfDigitsOfSum();
+        System.out.print(obj.productOfDigitsOfSum(1,99,17));
+
+    }
+}
